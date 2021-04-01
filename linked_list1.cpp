1d5aa292ae556e5dea390daf8309241f76d51fca
MishoCode <mihael_1998@abv.bg>
2021-04-01 19:08:23 +0300

    added file

---
+++ linked_list1.cpp
@@ -0,0 +1,67 @@
+#include<iostream>
+
+struct Node
+{
+    Node* next;
+    int data;
+
+    Node(int data, Node* next = nullptr)
+        :next(next), data(data)
+    {}
+    ~Node()
+    {
+        delete next;
+    }
+};
+
+Node* reverse(Node* head)
+{
+    Node* current = head, *prev = nullptr, *next = nullptr;
+    while(current != nullptr)
+    {
+        next = current->next;
+        current->next = prev;
+        prev = current;
+        current = next;
+    }
+    return prev;
+}
+void printList(Node* head)
+{
+    while(head!= nullptr)
+    {
+        std::cout<<head->data<<" ";
+        head = head->next;
+    }
+    std::cout<<std::endl;
+}
+Node* createSample1()
+{
+    return new Node(1, new Node(2, new Node(3, new Node(4, new Node(5)))));
+}
+Node* createSample2()
+{
+    return new Node(3);
+}
+Node* createSample3()
+{
+    return nullptr;
+}
+void testReverse()
+{
+    Node* test = createSample1();
+    std::cout<<"Before reverse: ";
+    printList(test);
+
+    std::cout<<"After reverse: ";
+    Node* result = reverse(test);
+    printList(result);
+
+    delete test;
+
+}
+int main()
+{
+    testReverse();
+    return 0;
+}
